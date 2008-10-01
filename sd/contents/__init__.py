889db548ca030825551b6ee5250e336d22c90a01
trollfot <trollfot@82af7df8-bc4b-4ebc-8022-2999806f7efb>
2008-10-01 19:37:32 +0000

    Eggification of sd.contents
    git-svn-id: svn+ssh://trollfot.org/svn/trollfot/projects/sd.contents/trunk@213 82af7df8-bc4b-4ebc-8022-2999806f7efb

--- sd/contents/__init__.py
+++ sd/contents/__init__.py
@@ -1,2 +1,8 @@
 # Base content package for sd
 # Includes mixins and base interfaces
+
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
